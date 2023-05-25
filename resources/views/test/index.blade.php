c59dd72f02bc0bc2da363c55f7c3ed92db6546b6
Lionel COTE <grcote7@gmail.com>
2023-05-25 18:10:14 +0200

    Fix: set css for blade view and app vuejs

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
     <div class="container" id="app">
+        <br><br>
         <h1 class="container text-blue-500 text-2xl font-bold mx-auto text-center">Liste des utilisateurs</h1>
         <users-list />
     </div>
