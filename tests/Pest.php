9f0dca8e662ee16d43427c330dd478c9b7a557a0
Lionel COTE <grcote7@gmail.com>
2023-05-25 09:59:09 +0200

    Feat: Clean breeze feature tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,19 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Test Case
-|--------------------------------------------------------------------------
-|
-| The closure you provide to your test functions is always bound to a specific PHPUnit test
-| case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
-| need to change it using the "uses()" function to bind a different classes or traits.
-|
-*/
+/**
+ * (ɔ) GrCOTE7 - 2022-2023
+ */
 
 uses(
-    Tests\TestCase::class,
-    // Illuminate\Foundation\Testing\RefreshDatabase::class,
+	Tests\TestCase::class,
+	Illuminate\Foundation\Testing\RefreshDatabase::class,
 )->in('Feature');
 
 /*
@@ -25,10 +18,10 @@ uses(
 | "expect()" function gives you access to a set of "expectations" methods that you can use
 | to assert different things. Of course, you may extend the Expectation API at any time.
 |
-*/
+ */
 
 expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
+	return $this->toBe(1);
 });
 
 /*
@@ -40,9 +33,9 @@ expect()->extend('toBeOne', function () {
 | project that you don't want to repeat in every file. Here you can also expose helpers as
 | global functions to help you to reduce the number of lines of code in your test files.
 |
-*/
+ */
 
 function something()
 {
-    // ..
+	// ..
 }
