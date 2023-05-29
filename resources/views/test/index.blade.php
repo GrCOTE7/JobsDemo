de8424d5089d60acd0b94cabfe4c3349b87ab93c
Lionel COTE <grcote7@gmail.com>
2023-05-29 14:09:08 +0200

    Fix: Line title h1

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -4,7 +4,13 @@
 
 @section('content')
     <div class="container" id="app">
-        <h1 class="container text-blue-500 text-2xl font-bold mx-auto text-center">Liste des utilisateurs (Vue)</h1>
+        <p class="h1 d-flex align-items-center justify-content-between">
+            <span class="text-blue-500 text-2xl font-bold">Liste des utilisateurs (Vue)</span>
+
+            <button class="btn btn-primary text-xl"><a href="{{ route('users.create') }}" style="color:white; font-size: 20px;" class="no-underline">+</a>
+            </button>
+        </p>
+
         <users-list-with-select />
     </div>
 @endsection
