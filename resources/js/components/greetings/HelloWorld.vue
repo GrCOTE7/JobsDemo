388a49a0727a589c7df0188d8b90db3651772512
Lionel COTE <grcote7@gmail.com>
2023-05-30 18:19:13 +0200

    Feat: Reservation Vols

--- resources/js/components/greetings/HelloWorld.vue
+++ resources/js/components/greetings/HelloWorld.vue
@@ -1,46 +1,78 @@
+<!--
+https://eugenkiss.github.io/7guis/tasks/#flight
+-->
+
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from 'vue'
 
-const euro = ref(1);
-const fr = ref(6.55957);
+const flightType = ref('one-way flight')
+const departureDate = ref(dateToString(new Date()))
+const returnDate = ref(departureDate.value)
 
-function round(num) {
-    var m = Number((Math.abs(num) * 100).toPrecision(15));
-    return ((Math.round(m) / 100) * Math.sign(num)).toFixed(2);
+const isReturn = computed(() => flightType.value === 'return flight')
+
+const canBook = computed(
+  () =>
+    !isReturn.value ||
+    stringToDate(returnDate.value) > stringToDate(departureDate.value)
+)
+
+function book() {
+  alert(
+    isReturn.value
+      ? `Vous avez réservé un vol aller-retour, avec un départ le ${departureDate.value} et un retour le ${returnDate.value}.`
+      : `Vous avez réservé un vol simple partant le ${departureDate.value}.`
+  )
 }
 
-function setEuro(e, v = e.target.value) {
-    euro.value = v;
-    fr.value = round(v * 6.55957, 2);
+function stringToDate(str) {
+  const [y, m, d] = str.split('-')
+  return new Date(+y, m - 1, +d)
 }
 
-function setFr(e, v = e.target.value) {
-    fr.value = v;
-    euro.value = round(v / 6.55957, 2);
+function dateToString(date) {
+  return (
+    date.getFullYear() +
+    '-' +
+    pad(date.getMonth() + 1) +
+    '-' +
+    pad(date.getDate())
+  )
+}
+
+function pad(n, s = String(n)) {
+  return s.length < 2 ? `0${s}` : s
 }
 </script>
 
 <template>
-    <form>
-        <div class="row">
-            <div class="col">
-                <label for="euro">€uros</label>
-                <input
-                    type="number"
-                    class="form-control"
-                    :value="euro"
-                    @keyup="setEuro"
-                />
-            </div>
-            <div class="col">
-                <label for="fr">Francs</label>
-                <input
-                    type="number"
-                    class="form-control"
-                    :value="fr"
-                    @keyup="setFr"
-                />
-            </div>
-        </div>
-    </form>
+  <select v-model="flightType">
+    <option value="one-way flight">Vol simple</option>
+    <option value="return flight">Vol aller-retour</option>
+  </select>
+
+  <input type="date" v-model="departureDate">
+  <input type="date" v-model="returnDate" :disabled="!isReturn">
+
+  <button :disabled="!canBook" @click="book">Réserver</button>
+
+  <p>{{ canBook ? '' : 'La date de retour doit être supérieure à la date de départ.' }}</p>
 </template>
+
+<style>
+select,
+input,
+button {
+  display: block;
+  margin: 0.5em 0;
+  font-size: 15px;
+}
+
+input[disabled] {
+  color: #999;
+}
+
+p {
+  color: red;
+}
+</style>
